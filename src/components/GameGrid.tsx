9226240e354c8aebb4eb09c013d34a7f35be0128
Mohammad_Mohammad <mm6dd.mh@gmail.com>
2023-05-27 22:11:07 +0100

    created custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,19 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-
-interface Games {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Games[];
-}
+import useGames from "../hooks/useGames";
+import { Text } from "@chakra-ui/react";
+import "./GamesGrid.css";
 
 function GameGrid() {
-  const [games, setGames] = useState<Games[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <>
       <ul>
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <li key={game.id}>
+            {game.name} {game.metacritic} {game.released.toString()}
+          </li>
         ))}
       </ul>
-      <div className="error-message">{error}</div>
+      {error && <Text>{error}</Text>}
     </>
   );
 }
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '2c3add5968ce4689bceae82cdb10e3a9'
+        key: 'ec220b8959e44c388d687b31ed55fd93'
     }
 })
