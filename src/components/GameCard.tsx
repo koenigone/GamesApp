1b73cfcf41a4b4ccc765381835bc06baf68c2554
Mohammad_Mohammad <mm6dd.mh@gmail.com>
2023-05-28 15:12:44 +0100

    optimised images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Games } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 import {
   Card,
   CardBody,
@@ -19,7 +20,7 @@ function GameCard({ game }: Prorps) {
   return (
     <>
       <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
           <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
