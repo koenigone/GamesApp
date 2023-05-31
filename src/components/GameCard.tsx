fce27e9f2993693d4396b4c8f935e4091d569ab8
Mohammad_Mohammad <mm6dd.mh@gmail.com>
2023-05-31 11:35:43 +0100

    a list of genres has been added

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -19,9 +19,12 @@ interface Prorps {
 function GameCard({ game }: Prorps) {
   return (
     <>
-      <Card>
-        <Image src={getCroppedImageUrl(game.background_image)} />
-        <CardBody>
+      <Card className="main-card">
+        <Image
+          src={getCroppedImageUrl(game.background_image)}
+          borderTopRadius={10}
+        />
+        <CardBody className="card-body">
           <Heading fontSize={"2xl"}>{game.name}</Heading>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
